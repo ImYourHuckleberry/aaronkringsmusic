052fa7280fabc59b1e410c731d17762f57b08ccf
tyler.krings <tyler.krings@daugherty.com>
2023-04-07 20:51:25 -0500

    video is go

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import aaronbackground from './aaronbackground.mp4';
-import aaronbackgroundWebM from './aaronbackgroundweb.webm';
-import aaronpng from './aaronpng.png'
+
 import './App.css';
 
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        {/* <video autoPlay muted loop id="myVideo">
-          <source src={aaronbackground} type="video/mp4"/>
-          <source src={aaronbackgroundWebM} type="video/webm"/>
-        </video> */}
-        <img src = {aaronpng} id="myVideo"/>
+        <video autoPlay muted loop id="myVideo">
+          <source src="https://aaronkringsmusic-storage-5a99e8b203307-staging.s3.amazonaws.com/aaronbackground.mp4" type="video/mp4"/>
+        </video>
         <div className='intro'>
           <p>Aaron Krings is a jazz artist, currently featured in the play Million Dollar Quartet.</p>
           <p>Opening April 20th, Wick Theater, Boca Raton, FL</p> </div>
