5110b7c5a8e2c35243e67b33688f927725874c96
tyler.krings <tyler.krings@daugherty.com>
2023-05-01 09:38:36 -0500

    refactor of front end components to make reuseable. Changed the title info. Changed some metadata to stop being about react. updated bio

---
+++ src/components/Home.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Card, CardContent, Button } from '@mui/material';
+import './Bio.css';
+import ContactCreateForm from '../ui-components/ContactCreateForm';
+import StyledContactFormCard from './StyledContactFormCard';
+import Bio from "./Bio"
+import Media from "./Media"
+import StandardCard from './StandardCard';
+
+const Home = () => {
+//    StandardCardProps
+//    const {title, italics, mediaSource, mediaType, link, alt, handleAction} = this.props;
+
+    return (
+        <div className="bio-container">
+            <div className='name-title'>
+            <h1 >Aaron Krings</h1>
+            <h2>Bassist Performer Educator</h2>
+            </div>
+            <StandardCard
+                title="New Album: "
+                italics="NOW HEAR THIS!"
+                mediaSource="https://aaronkringsmusic-storage-5a99e8b203307-staging.s3.amazonaws.com/nowhearthis.jpg"
+                mediaType="picture"
+                link='https://jonolejnik.bandcamp.com/album/now-hear-this'
+                alt="NOW HEAR THIS! Album Cover."
+                subtitle="Available Now"
+                callToAction="Buy"
+                />
+            <StandardCard
+                title="Now Playing in: "
+                italics="The Million Dollar Quartet"
+                mediaSource="https://aaronkringsmusic-storage-5a99e8b203307-staging.s3.amazonaws.com/MDQ.png"
+                mediaType="picture"
+                link='https://thewicktheatre.thundertix.com/events/197583'
+                alt="Million Dollar Quartet promotional picture"
+                subtitle="April 20 - May 14"
+                callToAction="Get Tickets"
+            />
+            <Bio />
+            <Media />
+            <StyledContactFormCard ><ContactCreateForm /></StyledContactFormCard>
+            <div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/StandardCard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Card, CardContent, Button } from '@mui/material';
+import './Bio.css';
+
+const StandardCard = (props) => {
+    const { title, italics, mediaSource, mediaType, link, alt, subtitle, callToAction } = props;
+    const handleButtonAlbum = () => {
+        window.open(
+            link,
+            '_blank' // <- This is what makes it open in a new window.
+        );
+    }
+
+    return (
+        <Card className="bio-card" id="current-gig">
+            <CardContent>
+                <h2>{title}<em>{italics}</em></h2>
+                <h3>{subtitle}</h3>
+                <div>
+                    {mediaType === "picture" && <img src={mediaSource} alt={alt}></img>}
+                </div>
+                <Button variant="contained" color="success" onClick={handleButtonAlbum}>
+                    {callToAction}
+                </Button>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default StandardCard;
